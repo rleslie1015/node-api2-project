e20eebaa130d3fb08abba7249adaeadef96daccc
Leslie Rodriguez <rleslie1015@gmail.com>
2020-07-07 05:39:19 -0500

    mvp

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -70,12 +70,13 @@ router.get('/:id/comments', (req, res) => {
   posts.findById(req.params.id)
   .then(post => {
     if (!post) {
-      return res.status(404).json({message: "The post with the specified ID does not exist." })
-    } 
+      res.status(404).json({message: "The post with the specified ID does not exist." })
+    } else {
+      return posts.findPostComments(req.params.id)
+    }
   })
-  return posts.findPostComments(req.params.id)
   .then((comments) => {
-    return res.status(200).json(comments)
+    res.status(200).json(comments)
   })
   .catch((err) => res.status(500).json({ error:  "The comments information could not be retrieved."}))
 });
